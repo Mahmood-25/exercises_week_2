dc76ea574d97d40a57959200e51f7fccb5073d31
Asheque Mahmood <mahmoodbtp@gmail.com>
2025-01-16 00:09:22 +0200

    documentation_added

--- src/main/java/main/Hedgehog.java
+++ src/main/java/main/Hedgehog.java
@@ -1,38 +1,43 @@
 package main;
 
-
-
+/**
+ * Class for Hedgehog
+ * @author asheque
+ */
 public class Hedgehog {
     private String name;
     private int age;
 
+    // Constructor
     public Hedgehog(String name, int age){
         this.name = name;
         this.age = age;
     }
-
+    // Get name
     public String getName(String name){
         return name;
     }
-
+    // Get age
     public int getAge(int age){
         return age;
     }
-
+    // Set name
     public void setName(String name){
         this.name = name;
     }
-
+    // Set age
     public void setAge(int age){
         this.age = age;
     }
 
+    // Methods
+    // Hedgehog can speak
     public void speak(){
         System.out.println("What does hedgehog say?");
         System.out.println();
         System.out.println("I am " + name + " and my age is " + age + ", but could you still give me input values?");
     }
-
+    // Hedgehog can run
     public void run(int laps){
         for (int i = 0; i < laps; i++){
             System.out.println(name + " runs really fast!");
